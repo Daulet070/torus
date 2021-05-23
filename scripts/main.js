32b092ae7fbc20c83fdb11f1b465d3f64f4b5a72
Daulet <anakhaev07@gmail.com>
2021-05-23 16:36:09 +0300

    Add space star and bg texture

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 import '../styles/style.css'
 
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 const scene = new THREE.Scene();
 
@@ -17,9 +18,34 @@ camera.position.setZ(30);
 renderer.render( scene, camera );
 
 const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
-const material = new THREE.MeshBasicMaterial( { color: 0xffff00, wireframe: true } );
+const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
 const torus = new THREE.Mesh( geometry, material );
 scene.add( torus );
+
+const pointLight = new THREE.PointLight(0xffffff)
+pointLight.position.set(5,5,5)
+
+const ambientLight = new THREE.AmbientLight(0xffffff)
+scene.add(pointLight, ambientLight)
+
+const lightHelper = new THREE.PointLightHelper(pointLight)
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(lightHelper, gridHelper)
+const controls = new OrbitControls(camera, renderer.domElement);
+
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial( { color: 0xffffff } );
+  const star = new THREE.Mesh( geometry, material );
+
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread( 100 ) );
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+Array(200).fill().forEach(addStar);
+
+const spaseTexture = new THREE.TextureLoader().load('../img/space.jpg');
+scene.background = spaseTexture;
 
 function animate() {
   requestAnimationFrame( animate );
@@ -27,6 +53,8 @@ function animate() {
   torus.rotation.x += 0.01;
   torus.rotation.y += 0.005;
   torus.rotation.z += 0.01;
+
+  controls.update();
 
   renderer.render( scene, camera );
 
